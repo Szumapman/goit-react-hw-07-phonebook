bd722f014ac57e0f9797b5fed8f0487e4e19e475
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-06 14:31:06 +0100

    update repo name for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/goit-react-hw-06-phonebook/',
+  base: '/goit-react-hw-07-phonebook/',
   plugins: [react()],
 })
