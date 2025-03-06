d5e91499127e670576bdec2fa6f8cb0678f8e57b
Paweł Szumański <pawel.szumanski@outlook.com>
2025-03-06 19:09:48 +0100

    refactor

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -6,8 +6,7 @@ import { setFilter } from '../../redux/filterSlice';
 
 export const Filter = () => {
     const dispach = useDispatch();
-    const filter = useSelector(selectFilter)
-    console.log(filter);
+    const filter = useSelector(selectFilter);
     const onFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         dispach(setFilter(event.target.value));
     }
